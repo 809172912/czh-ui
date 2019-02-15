7e77e89cf8c26e22ec39a01e53798d8fd10d9f98
打篮球好大汗 <809172912@qq.com>
2019-02-15 09:58:30 +0800

    [创建项目] czh-ui

---
+++ src/components/TopBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <span>
+      你好，这是czhUI
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+  }
+}
+</script>
+<style scoped>
+  .hello {
+    font-size: 50px;
+    color: red;
+  }
+</style>
