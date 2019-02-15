a21e6979d67c29069a50fe3be3dabac212612e04
Administrator <809172912@qq.com>
2019-02-15 16:43:04 +0800

    [开发] 修改打包入口，同时支持全局加载和按需加载

--- src/components/CButton.vue
+++ src/components/CButton.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="hello">
     <span>
-      你好，这是czhUI
+      你好，这是czhUI的button组件
     </span>
   </div>
 </template>
 <script>
-export default {
-  name: 'TopBar',
-  props: {
+  export default {
+    name: 'CButton',
+    props: {
+    }
   }
-}
 </script>
 <style scoped>
   .hello {
--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,27 @@
-import Vue from 'vue'
 import TopBar from './TopBar.vue'
+import CButton from './CButton.vue'
+
 const Components = {
-    TopBar
+  TopBar,
+  CButton
 }
 
-Object.keys(Components).forEach(name => {
+const install = function (Vue) {
+  Object.keys(Components).forEach(name => {
     Vue.component(name, Components[name])
-})
+  })
+}
 
-export default Components
+// auto install
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export {
+  TopBar,
+  CButton
+}
+
+export default {
+  install
+}
